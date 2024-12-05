b5a31ff272bf3f24bb2f5bd7c5c86049518de06f
Long <85337233+tranlongiuh@users.noreply.github.com>
2024-12-06 03:55:14 +0700

    test2

--- src/main/java/com/iuh/canteen/config/WebSecurityConfig.java
+++ src/main/java/com/iuh/canteen/config/WebSecurityConfig.java
@@ -86,7 +86,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("https://localhost:3000/"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
