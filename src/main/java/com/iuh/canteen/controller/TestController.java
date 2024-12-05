94d48721d43ea8790c5bb8b13b822e5bd4bdc6b0
Long <85337233+tranlongiuh@users.noreply.github.com>
2024-12-06 04:23:39 +0700

    ...

--- src/main/java/com/iuh/canteen/controller/TestController.java
+++ src/main/java/com/iuh/canteen/controller/TestController.java
@@ -1,9 +1,5 @@
 package com.iuh.canteen.controller;
 
-import com.iuh.canteen.dto.OrdersDTO;
-import com.iuh.canteen.repository.OrdersRepository;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test/api")
 public class TestController {
 
-    @Autowired
-    private OrdersRepository ordersRepository;
 
-    @Autowired
-    private ModelMapper modelMapper;
 
     @GetMapping
     ResponseEntity<?> testApi() {
 
-        return ResponseEntity.ok(modelMapper.map(ordersRepository.findByOrderItemsId(15L)
-                , OrdersDTO.class));
+        return ResponseEntity.ok("testApi");
     }
 }
